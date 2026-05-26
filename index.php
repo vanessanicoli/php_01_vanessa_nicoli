463a15dc7755903f608b139517e9384567536518
Vanessa Nicolì <nicoli.vanessa1996@gmail.com>
2026-05-27 00:45:13 +0200

    Terminato Selfwork PHP 01

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+ 
+// - Definisci 4 variabili:
+//     - Integer
+//     - Float
+//     - String
+//     - Boolean
+// - A schermo, fai comparire il tipo di dato delle varie variabili.
+// - Trasforma quelle variabili in costanti, utilizzando le best practice viste a lezione.
+// - Pusha il codice su GitHub con il nome php_01_nome_cognome. 
+
+
+$integer = 10;
+$float = 10.5;
+$string = "Stringa";
+$boolean = true;
+
+var_dump($integer);
+var_dump($float);
+var_dump($string);
+var_dump($boolean);
+
+// COSTANTI
+const INTEGER_NUMBER = 10;
+const FLOAT_NUMBER = 10.5;
+const TEXT_STRING = "Stringa";
+const IS_ACTIVE = true;
+
+var_dump(INTEGER_NUMBER);
+var_dump(FLOAT_NUMBER);
+var_dump(TEXT_STRING);
+var_dump(IS_ACTIVE);
